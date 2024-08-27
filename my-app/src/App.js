34a43dce18d276c0d01c267e49b853fe7ce803e6
Luisa99jw <lufitha1999@gmail.com>
2024-08-27 09:30:32 -0500

    componentes react
    creaciòn de componentes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import WelcomeMessage from "./welcomeMessage";
+import PresentacionNombre from "./presentacionNombre";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-         Spin animation.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WelcomeMessage/>
+      <PresentacionNombre/>
     </div>
+    
   );
 }
 
