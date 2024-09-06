0126de25d368becc2d8805080e9f976153d76088
Luisa99jw <lufitha1999@gmail.com>
2024-09-05 23:40:03 -0500

    cambios_taller_2

---
+++ taller_2/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import WelcomeMessage from "./welcomeMessage";
+import PresentacionNombre from "./presentacionNombre";
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage/>
+      <PresentacionNombre/>
+    </div>
+    
+  );
+}
+
+export default App;
